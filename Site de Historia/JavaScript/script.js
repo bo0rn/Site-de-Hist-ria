58bdc88bad8376ea62892c65188183706bfd9631
Emillyfc <Camposemilly903@gmail.com>
2025-06-03 00:47:25 -0300

    Add files via upload

---
+++ Site de Historia/JavaScript/script.js
@@ -0,0 +1,8 @@
+function menu() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
